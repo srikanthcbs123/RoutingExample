ca15b5a6d8c3518e40f5f835c9e1b058d140de00
srikanthcbs123 <srikanthcbs123@gmail.com>
2025-03-05 16:02:41 +0530

    Add project files.

---
+++ RoutingExample/Controllers/EmployeesController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RoutingExample.Controllers
+{
+    [Route("api/[controller]")]//parent route declare here
+    [ApiController]//attributes.
+    public class EmployeesController : ControllerBase
+    {
+
+
+        [HttpGet]//api methods
+        [Route("GetEmployee/{sipId}/{siphealthcheckId}")]//children routename define here.
+
+        public async Task<IActionResult> EmployeeDetails([FromRoute] string sipId, [FromRoute] string siphealthcheckId)
+        {
+            var result = sipId + siphealthcheckId;
+            //In future we can create some reporsitroty classes we can return database data
+            return Ok(result);
+        }
+
+        [HttpGet]//api methods
+        [Route("GetEmployee/{sipId}/{siphealthcheckId}/{a}/{b}/{c}")]//children routename define here.
+
+        public async Task<IActionResult> EmployeeDetails([FromRoute] string sipId, [FromRoute] string siphealthcheckId,[FromRoute] string a, [FromRoute] string b, [FromRoute] string c)
+     
+        {
+            var result = sipId + siphealthcheckId+a+b+c;
+            //In future we can create some reporsitroty classes we can return database data
+            return Ok(result);
+        }
+
+        private string GetName()
+        {
+            return "hello";
+        }
+    }
+}
---
+++ RoutingExample/Models/employee.cs
@@ -0,0 +1,16 @@
+﻿namespace RoutingExample.Models
+{
+    public class employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int EmployeeId { get; set; } = 1;
+        public int EmployeeName { get; set; }
+        public int EmployeeDescription { get; set; }
+        public int EmployeeAge { get; set; }
+        public int EmployeeGender { get; set; }
+        public int EmployeePhone { get; set; }
+        public int EmployeePhoneNumber { get; set; }
+    }
+}
