b38c20de53d89011d384309d0f382f899678c735
srikanthcbs123 <srikanthcbs123@gmail.com>
2025-03-07 15:37:54 +0530

    Userstory_1467726 Created Api methods

--- RoutingExample/Controllers/EmployeesController.cs
+++ RoutingExample/Controllers/EmployeesController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using RoutingExample.Models;
 
 namespace RoutingExample.Controllers
 {
@@ -8,10 +9,10 @@ namespace RoutingExample.Controllers
     public class EmployeesController : ControllerBase
     {
 
-
-        [HttpGet]//api methods
-        [Route("GetEmployee/{sipId}/{siphealthcheckId}")]//children routename define here.
-
+    
+        //[HttpGet]//api methods
+       // [Route("GetEmployee/{sipId}/{siphealthcheckId}")]//children routename define here.
+        [HttpGet("GetEmployee/{sipId}/{siphealthcheckId}")]//his is the shortcut way of writting httpmethod and route in single line
         public async Task<IActionResult> EmployeeDetails([FromRoute] string sipId, [FromRoute] string siphealthcheckId)
         {
             var result = sipId + siphealthcheckId;
@@ -29,6 +30,73 @@ namespace RoutingExample.Controllers
             //In future we can create some reporsitroty classes we can return database data
             return Ok(result);
         }
+
+
+        [HttpGet("GetEmployeesQueryString")]
+        public string GetData([FromQuery] string a, [FromQuery] string b, [FromQuery] string c, [FromQuery] string d, [FromQuery] string e)
+        {
+            return a + b + c + d + e;
+
+        }
+        [HttpGet("GetEmployeesSinleQueryString")]//sychronus programming methods
+        public string GetDatawithsqingleparam([FromQuery] string a)
+        {
+            return a;
+
+        }
+        [HttpGet("Sip-comment/{SiphealthCheckDetailId}")]//asynchronous programming methods.
+        public async Task<IActionResult> SipComment([FromRoute] long SiphealthCheckDetailId)
+        {
+            var checkType = SiphealthCheckDetailId;
+            return Ok(checkType);
+        }
+        [HttpGet("SipEngagementSelection/healthCheckDetailId/{healthCheckDetailId}/questionId/{questionId}")]
+        public async Task<IActionResult> SipGet([FromRoute] int healthCheckDetailId, [FromRoute] int questionId)
+        {
+
+            return Ok(healthCheckDetailId  + questionId);
+
+        }
+
+
+        //Querystring means route starts with ?(question mark firsttimeonly)
+        // Route: GET /api/RealtimeRoutes/search?name=Phone&category=Electronics
+        [HttpGet("SearchProducts1")]
+        public IActionResult SearchProducts1([FromQuery] string name, [FromQuery] string category)
+        {
+            var result = "Searching for" + name + "in" + category + "category.";
+            //the below one is the string concadination
+            return Ok($"Searching for {name} in {category} category.");
+        }
+        // Route: GET /api/RealtimeRoutes/SearchProducts2?name=Phone
+        [HttpGet("SearchProducts2")]
+        public IActionResult SearchProducts2([FromQuery] string name)
+        {
+            //the below one is the string concadination
+            return Ok($"Searching for {name}.");
+        }
+        //Example: Combining [FromRoute] and [FromQuery]
+        // Route: http://localhost:5228/api/RealtimeRoutes/123?status=pen
+        [HttpGet("{orderId:int}")]
+        public IActionResult GetOrderDetails(
+            [FromRoute] int orderId,
+            [FromQuery] string status)
+        {
+            return Ok($"Order ID: {orderId}, Status: {status}");
+        }
+
+        //Complex binding with [FromQuery]
+        // Route: http://localhost:5228/api/RealtimeRoutes/filter?Name=hyd&Category=book&MinPrice=10&MaxPrice=20
+        [HttpGet("filter")]
+        public IActionResult FilterProducts([FromQuery] ProductFilter filter)
+        {
+            return Ok($"Filtering {filter.Name} with min price {filter.MinPrice}.");
+        }
+
+
+
+
+
 
         private string GetName()
         {
---
+++ RoutingExample/Models/ProductFilter.cs
@@ -0,0 +1,10 @@
+﻿namespace RoutingExample.Models
+{
+    public class ProductFilter
+    {
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public int? MinPrice { get; set; }
+        public int? MaxPrice { get; set; }
+    }
+}
